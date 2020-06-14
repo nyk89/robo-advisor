a412ae4066893f010c50c3f8b6e2c2cb81dec53f
KML122089 <65563104+KML122089@users.noreply.github.com>
2020-06-14 19:23:49 -0400

    write example data to csv

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -42,19 +42,33 @@ for date in dates:
 recent_high = max(high_prices)
 recent_low = min(low_prices)
 
+import datetime
+now = datetime.datetime.now()
 
 import csv
 import json
-import datetime
-now = datetime.datetime.now()
+import os
+
 
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
+csv_file_path = "data/prices.csv" # a relative filepath
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "../data/prices.csv")
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
+print("REQUESTING STOCK MARKET DATA")
 print("REQUEST AT:", now.strftime("%Y-%m-%d %H:%M %p")) #can I change this from 24 hour clock?
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
@@ -65,18 +79,9 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
-print("WRITING DATA TO CSV...")
+print(f"WRITING DATA TO CSV: {csv_file_path}")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
 
 
-csv_file_path = "data/prices.csv" # a relative filepath
-
-with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
-    writer.writeheader() # uses fieldnames set above
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
