5681c276c4262a92cb765b7ac35c12dd0a211040
KML122089 <65563104+KML122089@users.noreply.github.com>
2020-06-14 13:48:42 -0400

    latest close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -15,11 +15,15 @@ response = requests.get(request_url)
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"]["2020-06-12"]["4. close"]
+
 # breakpoint()
 
 # print(type(response)) # <class 'requests.models.Response'>
 # print(response.status_code) #200
 # print(response.text)
+
 
 import datetime
 now = datetime.datetime.now()
@@ -31,7 +35,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT:", now.strftime("%Y-%m-%d %H:%M %p")) #can I change this from 24 hour clock?
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {latest_close}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
