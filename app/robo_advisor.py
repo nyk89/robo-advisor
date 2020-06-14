8248bdfb4f890811e520cca0f46d44449a1ca93b
KML122089 <65563104+KML122089@users.noreply.github.com>
2020-06-14 18:59:20 -0400

    recent low

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -31,12 +31,16 @@ latest_close = tsd[latest_day]["4. close"]
 
 
 high_prices = []
+low_prices = []
 
 for date in dates:
     high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
     high_prices.append(float(high_price))
+    low_prices.append(float(low_price))
 
 recent_high = max(high_prices)
+recent_low = min(low_prices)
 
 # print(type(response)) # <class 'requests.models.Response'>
 # print(response.status_code) #200
@@ -59,7 +63,7 @@ print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float((latest_close)))}")
 print(f"RECENT HIGH: {to_usd(float((recent_high)))}") # 100 day max high
-print("RECENT LOW: $99,000.00") 
+print(f"RECENT LOW: {to_usd(float((recent_low)))}") # 100 day min low
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
