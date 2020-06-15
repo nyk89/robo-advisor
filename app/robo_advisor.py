c6b2e940c36f6c4d0a61de24fe328f1e10ad7128
KML122089 <65563104+KML122089@users.noreply.github.com>
2020-06-14 21:11:48 -0400

    recommendations added

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,21 +1,27 @@
 import requests
 import json
+import os
+import csv
+import datetime
+from dotenv import load_dotenv
 
-#
-# INFO INPUTS
-#
+load_dotenv() #> loads contents of the .env file into the script's environment
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+def to_usd(my_price):
+    return f"${my_price:,.2f}" #> $12,000.71
+
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+
+#input("ENTER TICKER HERE: ")
+symbol = input("ENTER TICKER HERE: ")
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 
 response = requests.get(request_url)
-#
-#
-#
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-
 
 tsd = parsed_response["Time Series (Daily)"]
 
@@ -24,11 +30,6 @@ dates = list(tsd.keys()) # to do make sorted list so most recent date is first
 latest_day = dates[0]
 
 latest_close = tsd[latest_day]["4. close"]
-
-
-# high_prices = [10, 20, 30, 5]
-# recent_high = max(high_prices)
-
 
 high_prices = []
 low_prices = []
@@ -42,12 +43,7 @@ for date in dates:
 recent_high = max(high_prices)
 recent_low = min(low_prices)
 
-import datetime
 now = datetime.datetime.now()
-
-import csv
-import json
-import os
 
 csv_file_path = "data/prices.csv" # a relative filepath
 
@@ -69,13 +65,20 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
         "volume": daily_prices["5. volume"]
     })
 
-        
+if float(latest_close) <= recent_low:
+    recommendation = "BUY IT WARREN"
+    reason = "EQUITY SALE ON ISLE FIVE"
 
-def to_usd(my_price):
-    return f"${my_price:,.2f}" #> $12,000.71
+if float(latest_close) >= recent_high:
+    recommendation = "SELL IT MR. BUFFET"
+    reason = "UNPRECIDENTED HIGHS, CASH IN"
+
+if recent_low < float(latest_close) < recent_high:
+    recommendation = "HOLD IT MR. BUFFET"
+    reason = "NOTHING INTERESTING TO SEE HERE, KEEP HOLDING"
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA")
 print("REQUEST AT:", now.strftime("%Y-%m-%d %H:%M %p")) #can I change this from 24 hour clock?
@@ -85,8 +88,8 @@ print(f"LATEST CLOSE: {to_usd(float((latest_close)))}")
 print(f"RECENT HIGH: {to_usd(float((recent_high)))}") # 100 day max high
 print(f"RECENT LOW: {to_usd(float((recent_low)))}") # 100 day min low
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
+print(f"RECOMMENDATION: {recommendation}")
+print(f"RECOMMENDATION REASON: {reason}")
 print("-------------------------")
 print(f"WRITING DATA TO CSV: {csv_file_path}")
 print("-------------------------")
